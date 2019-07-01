67e148a6d789ac9cf8262b50766c9cc93e013470
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-01 16:36:51 -0700

    Created models to store data

---
+++ android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,26 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+
+    //attributes
+    public String body;
+    public long uid; //database ID for the tweet
+    public String createdAt;
+    public User user;
+
+    //deserialize the JSON
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        //extract all the values from JSON
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+}
