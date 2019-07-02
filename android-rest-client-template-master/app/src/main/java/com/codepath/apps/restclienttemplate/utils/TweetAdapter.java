951e127126ac4d133852cc6b9880f2bf94d86046
PabloMoreno2000 <pamta2000@gmail.com>
2019-07-01 17:45:11 -0700

    Started adapter for the tweets

---
+++ android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/utils/TweetAdapter.java
@@ -0,0 +1,73 @@
+package com.codepath.apps.restclienttemplate.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
+    private List<Tweet> mTweets;
+
+    //pass in the tweets array in the constructor
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+    //for each row, ifnlate the layout and cache referencies into viewholder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+
+        return viewHolder;
+    }
+
+
+    //bind the values based on the position of the element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        //get the data according to position
+        Tweet tweet = mTweets.get(i);
+
+        //populate the  views according to this data
+        viewHolder.tvUsername.setText(tweet.user.name);
+        viewHolder.tvBody.setText(tweet.body);
+    }
+
+    //crete viewholder class
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            ivProfileImage =itemView.findViewById(R.id.ivUser);
+
+            tvUsername = itemView.findViewById(R.id.tvuserName);
+
+            tvBody = itemView.findViewById(R.id.tvBody);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+}
